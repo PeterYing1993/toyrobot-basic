5ee35438d9d2fb3495cd9ae64aadc00f1471349d
PeterYing1993 <peteryingfrontend@gmail.com>
2019-07-14 20:04:23 +1000

    fix THIS PROBLEM

--- robot.js
+++ robot.js
@@ -1,3 +1,49 @@
+// class Game {
+//     constructor(mapSize, initPosition, icon) {
+//         this.state = {
+//             robotPosition: initPosition,
+//             mapSize: mapSize,
+//             icon: icon
+//         };
+//     }
+
+//     availablePosition(newPosition, mapSize) {
+//         if (newPosition >= 0 && newPosition < mapSize) {
+//             return true;
+//         } else {
+//             return false;
+//         }
+//     }
+    
+//     move(newPosition) {
+//         if (this.availablePosition(newPosition, this.state.mapSize)) {
+//             this.state.robotPosition = newPosition;
+//             this.render();
+//             return true;
+//         } else {
+//             return false;
+//         }
+//     }
+    
+//     render() {
+//         var mapCells = document.querySelectorAll('.map-cell');
+//         mapCells.forEach(function(aCell, index) {
+//             if (index === game.state.robotPosition) {
+//                 aCell.innerHTML = game.state.icon;
+//             } else {
+//                 aCell.innerHTML = '';
+//             }
+//         })
+//     }
+    
+//     onCommandRight() {
+//         this.move(this.state.robotPosition + 1);
+//     }
+    
+// }
+
+// var game = new Game(5, 2, 'R');
+// game.render();
 class Game {
     constructor(mapSize, initPosition, icon) {
         this.state = {
@@ -14,7 +60,7 @@ class Game {
             return false;
         }
     }
-    
+
     move(newPosition) {
         if (this.availablePosition(newPosition, this.state.mapSize)) {
             this.state.robotPosition = newPosition;
@@ -24,23 +70,31 @@ class Game {
             return false;
         }
     }
-    
+
     render() {
         var mapCells = document.querySelectorAll('.map-cell');
-        mapCells.forEach(function(aCell, index) {
-            if (index === this.state.robotPosition) {
+        // for (var index = 0; index < mapCells.length; index++) {
+        //     if (index === this.state.robotPosition) {
+        //         aCell.innerHTML = this.state.icon;
+        //     } else {
+        //         aCell.innerHTML = '';
+        //     }
+        // }
+        mapCells.forEach((aCell, index) => {
+            if (index === this.state.三robotPosition) {
                 aCell.innerHTML = this.state.icon;
             } else {
                 aCell.innerHTML = '';
             }
-        })
+        });
     }
-    
+
     onCommandRight() {
         this.move(this.state.robotPosition + 1);
     }
-    
+
 }
+
 
 var game = new Game(5, 2, 'R');
 game.render();
